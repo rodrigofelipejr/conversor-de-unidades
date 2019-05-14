9d0ff3704d55b18899b176732981d0b00713d5a8
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2019-05-13 23:01:45 -0300

    add new validations

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,23 @@
 $(function () {
+  const btnConvert = '#btnConvert'
+  const editMilhas = '#editMilhas'
+
+  //only numbers
+  $(editMilhas).keyup(() => {
+    $(editMilhas).val($(editMilhas).val().replace(/[^0-9]/g, ''))
+  })
+
   $('#btnConvert').click(() => {
-    const milhas = parseFloat($('#editMilhas').val())
+    const milhas = parseFloat($(editMilhas).val())
+
+    if (isNaN(milhas)) {
+      $(editMilhas).focus()
+      $(editMilhas).next().addClass('form-text error')
+      return 
+    } 
+
     const metros = milhas * 1609.34
     $('#editMetros').val((metros.toFixed(2).toString().replace('.', ',')))
+    console.log('end click');
   })
 })
