86b8cfeae305bfd4c11d99c81e36ac4fcdac5e1a
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2019-05-18 18:31:58 -0300

    enabling change button part 1/3

--- js/calc.js
+++ js/calc.js
@@ -1,42 +1,111 @@
 $(function () {
-  const btn_convert = '#btnConvert'
-  const edit_milhas = '#editMilhas'
-  const edit_metros = '#editMetros'
+  // initializing
+  main();
 
-  //only numbers
-  $(edit_milhas).keyup(() => {
-    $(edit_milhas).val($(edit_milhas).val().replace(/[^0-9]/g, ''))
+  // button change
+  fnChangeForm()
 
-    if ($(edit_milhas).val().length > 0) {
-      $(edit_milhas).next().removeClass('error')
+  // format form
+  fnRebuildingForm()
+})
+
+const main = () => {
+  console.clear()
+
+  let edit_first_Id = document.querySelector('form .form-group:nth-of-type(1) input[type="text"]').id
+  let edit_first = '#editMilhas'
+  let edit_second = '#editMetros'
+
+  if (edit_first_Id === 'editMetros') {
+    edit_first = '#editMetros'
+    edit_second = '#editMilhas'
+  }
+
+  console.log(`edit_first ${edit_first}`);
+  console.log(`edit_second ${edit_second}`);
+
+  // enter only numbers
+  $(edit_first).keyup(() => {
+    $(edit_first).val($(edit_first).val().replace(/[^0-9]/g, ''))
+
+    if ($(edit_first).val().length > 0) {
+      $(edit_first).next().removeClass('error')
     }
   })
 
-  //clean the second input on the output of the first
-  $(edit_milhas).blur(() => {
-    if ($(edit_milhas).val().length === 0) { $(edit_metros).val('') }
+  // clean the second input on the output of the first
+  $(edit_first).blur(() => {
+    if ($(edit_first).val().length === 0) {
+      $(edit_second).val('')
+    }
   })
 
-  //hide validation
-  const hideError = () => {
-    $(edit_milhas).next().removeClass('error')
-  }
+  // ===========================
+  // conversion
+  // ================
+  $('#btnConvert').click(() => {
+    const inputValue = parseFloat($(edit_first).val())
 
-  //click button
-  $(btn_convert).click(() => {
-    const milhas = parseFloat($(edit_milhas).val())
+    // validation for null value
+    if (isNaN(inputValue)) {
+      $(edit_first).focus()
+      $(edit_first).next().addClass('error')
+      $(edit_second).val('')
 
-    //validation
-    if (isNaN(milhas)) {
-      $(edit_milhas).focus()
-      $(edit_milhas).next().addClass('error')
-      $(edit_metros).val('')
-      setTimeout(hideError, 3000)
+      // hide validation
+      setTimeout(() => {
+        $(edit_first).next().removeClass('error')
+      }, 3000)
+
       return
     }
 
-    //calc
-    const metros = milhas * 1609.34
-    $(edit_metros).val((metros.toFixed(2).toString().replace('.', ',')))
+    // conversion calculates 
+    let resultValue = (edit_first_Id === 'editMilhas') ? inputValue * 1609.34 : inputValue / 1609.34
+
+    // setting the result
+    $(edit_second).val((resultValue.toFixed(2).toString().replace('.', ',')))
   })
-})
+}
+
+const fnRebuildingForm = () => {
+  // creating selectors
+  const firstElement = 'form .form-group:nth-of-type(1) input[type="text"]'
+  const secondElement = 'form .form-group:nth-of-type(2) input[type="text"]'
+
+  // enabling and disabling elements
+  document.querySelector(firstElement).removeAttribute('readonly')
+  document.querySelector(firstElement).value = ''
+
+  document.querySelector(secondElement).setAttribute('readonly', '')
+  document.querySelector(secondElement).value = ''
+}
+
+const fnChangeForm = () => {
+  $(document).on('click', '.btn.btnChange', () => {
+    // getting form
+    elementForm = document.querySelector('form')
+
+    // getting elements
+    const firstSelector = 'form .form-group:nth-of-type(1)'
+    const firstElement = document.querySelector(firstSelector)
+    const firstElementHTML = document.querySelector(firstSelector).cloneNode(true)
+
+    // getting elements
+    const secondSelector = 'form .form-group:nth-of-type(2)'
+    const secondElement = document.querySelector(secondSelector)
+    const secondElementHTML = document.querySelector(secondSelector).cloneNode(true)
+
+    // removing elements
+    firstElement.remove()
+    secondElement.remove()
+
+    // add new elements
+    elementForm.insertBefore(secondElementHTML, document.querySelector(firstSelector) || null)
+    elementForm.insertBefore(firstElementHTML, document.querySelector(secondSelector) || null)
+
+    // rearranging form
+    fnRebuildingForm()
+    main()
+  })
+}
